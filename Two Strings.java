d07b7c380587f501392ad5744afcb306168ef4e5
Antimony8 <97124775+Antimony8@users.noreply.github.com>
2023-10-18 12:46:47 -0400

    Create Two Strings.java

---
+++ Two Strings.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static String twoStrings(String s1, String s2) {
+        //base case
+        if (s1.isEmpty() || s2.isEmpty()) {
+            return "NO";
+        }
+        //check if first character of s1 is in s2
+        if (s2.contains(s1.substring(0, 1))) {
+            return "YES";
+        //recursive call
+        } else {
+            return twoStrings(s1.substring(1), s2);
+        }
+    }
+}
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
